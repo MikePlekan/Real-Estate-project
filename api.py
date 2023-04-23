eca664402767e3390680f8b9a8423531857a56c5
MikePlekan <70487027+MikePlekan@users.noreply.github.com>
2023-04-23 17:34:25 +0000

    get data into dataframe

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 import http.client
 import json
 import pprint
+import pandas as pd
+
 # conn = http.client.HTTPSConnection("api.gateway.attomdata.com")
 
 # headers = { 
@@ -24,6 +26,20 @@ with open('data.json', 'r') as openfile:
 [x.pop("location") for x in data['property']]
 [x.pop("lot") for x in data['property']]
 [x.pop("vintage") for x in data['property']]
+df=pd.DataFrame(columns=['countrySubd', 'locality', 'attomId','saleTransDate','proptype','yearbuilt'])
+sales=pd.DataFrame(columns=['saleamt','attomId'])
+for x in data['property']:
+    temp=pd.DataFrame({'countrySubd':[x['address'].get('countrySubd')],
+                        'locality':[x['address'].get('locality')],
+                        'attomId':[x['identifier'].get('attomId')],
+                        'saleTransDate':[x['sale'].get('saleTransDate')],
+                        'proptype':[x['summary'].get('proptype')],
+                        'yearbuilt':[x['summary'].get('yearbuilt')]})
+    df=pd.concat([df,temp],ignore_index=True)
+    temps=pd.DataFrame({'saleamt':[x['sale']['amount'].get('saleamt')],'attomId':[x['identifier'].get('attomId')]})
+    sales=pd.concat([sales,temps],ignore_index=True)
+
+df.plot(x='yearbuilt',y='saleTransDate',kind='scatter')
 pp = pprint.PrettyPrinter(indent=2)
 with open("dict1.txt", "w") as outfile:
     outfile.write(pp.pformat(data))
