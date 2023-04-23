e75497950badbc50455fed0c5cdb93b0e107999a
MikePlekan <70487027+MikePlekan@users.noreply.github.com>
2023-04-23 02:04:43 +0000

    cleaning data

--- api.py
+++ api.py
@@ -1,18 +1,30 @@
 import http.client
 import json
 import pprint
-conn = http.client.HTTPSConnection("api.gateway.attomdata.com")
+# conn = http.client.HTTPSConnection("api.gateway.attomdata.com")
 
-headers = { 
-    'accept': "application/json", 
-    'apikey': "", 
-} 
+# headers = { 
+#     'accept': "application/json", 
+#     'apikey': "2b1e86b638620bf2404521e6e9e1b19e", 
+# } 
 
-conn.request("GET", "/propertyapi/v1.0.0/sale/snapshot?latitude=30.2500&longitude=-97.7500&radius=200&orderBy=saleAmt+desc&pageSize=50", headers=headers)
+# conn.request("GET", "/propertyapi/v1.0.0/sale/snapshot?latitude=30.2500&longitude=-97.7500&radius=200&orderBy=saleAmt+desc&pageSize=50", headers=headers)
 
-res = conn.getresponse()
-data = res.read()
-data=json.loads(data.decode("utf-8"))
+# res = conn.getresponse()
+# data = res.read()
+# with open("data.json", "w") as outfile:
+#     outfile.write(data.decode("utf-8"))
+# data=json.loads(data.decode("utf-8"))
 
+# Opening JSON file
+with open('data.json', 'r') as openfile:
+ 
+    # Reading from json file
+    data = json.load(openfile)
+[x.pop("location") for x in data['property']]
+[x.pop("lot") for x in data['property']]
+[x.pop("vintage") for x in data['property']]
 pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(data)
+with open("dict1.txt", "w") as outfile:
+    outfile.write(pp.pformat(data))
+
